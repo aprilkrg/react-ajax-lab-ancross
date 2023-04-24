f2642d43aa717ffe711e8f500a928f44c896c42c
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-24 15:56:27 -0600

    add react-scroll-to-top package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
+import ScrollToTop from 'react-scroll-to-top';
 import './App.css';
 import DisplayCards from './components/DisplayCards.js'
 
@@ -88,6 +89,7 @@ export default function App() {
                     handleClick={handleClick}
                 />
             </div>
+            <ScrollToTop smooth />
         </>
     )
 }
