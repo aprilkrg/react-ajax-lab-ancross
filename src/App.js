d090453aa761162bf45346a5c46b050191235005
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-24 13:12:15 -0600

    functional async/await solve with favoriting

--- src/App.js
+++ src/App.js
@@ -1,98 +1,96 @@
-import {
-  useEffect,
-  useState
-} from 'react'
-import DisplayCards from './DisplayCards'
-import axios from 'axios'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css';
+import DisplayCards from './components/DisplayCards.js'
 
 function App() {
-  const [data, setData] = useState({ villagers: [] })
-  const [search, setSearch] = useState('')
-  const [faves, setFaves] = useState([])
+	// STATE
+	const [data, setData] = useState({ villagers: [] })
+	const [search, setSearch] = useState("")
+	const [faves, setFaves] = useState([])
 
-  useEffect(() => {
-    // to avoid asycning the useEffect's cb, we will define a async function within the useEffect's cb
-    // Immediately invoked function expression
-    // IIFE (ifies) 
-    // you can use a named function too
-    (async () => {
-      try {
-        const response = await axios.get('http://acnhapi.com/v1/villagers/')
-        // console.log(response.data)
-        // const villagerArray = Object.values(response.data)
-        // console.log(villagerArray)
-        setData({ villagers: Object.values(response.data) })
-      } catch(err) {
-        console.warn(err)
-      }
-    })() // immediately invoked
-  }, [])
+	// LIFECYCLE
+	useEffect(() => {
+		try {
+			async function fetchData() {
 
-  const handleChange = e => setSearch(e.target.value)
+				const response = await axios.get('http://acnhapi.com/v1/villagers/')
+				// console.log("VILLAGERS:", response.data) // obj of objs
+				// Object.values() turns data into arr of objs
+				// console.log("OBJECTS:", Object.values(response.data))
+				// set state
+				setData({ villagers: Object.values(response.data) })
+			}
+			fetchData()
+		} catch (err) {
+			console.warn(err)
+		}
+	}, [])
 
-  const handleClick = villager =>  {
-    // make sure the villager is not in the faves array
-    if (!faves.includes(villager)) {
-      setFaves([...faves, villager])
-    } else {
-      // find the villager's index in the faves state
-      const villagerIndex = faves.indexOf(villager)
-      // make a copy of the faves array, so we can mutate it
-      const favesCopy = [...faves]
-      // splice the villager out and setState
-      console.log(villagerIndex, favesCopy)
-      setFaves(favesCopy.splice(villagerIndex, 1))
-    }
-  }
+	// FUNCTIONS
+	// const villagerList = data.villagers.map((villager) => {
+	// 	return <li>{villager.name["name-USen"]}</li>
+	// })
+	const handleChange = (e) => {
+		setSearch(e.target.value)
+	}
 
-  // const getFilteredVillagers = () => {
-  //   const searchTerm = search.toLowerCase()
-  //   return data.villagers.filter(villager => {
-  //     const lowerCaseName = villager.name['name-USen'].toLowerCase()
-  //     return lowerCaseName.includes(searchTerm)
-  //   })
-  // }
+	const filteredVillagers = data.villagers.filter(villager => {
+		const loweredName = villager.name["name-USen"].toLowerCase()
+		return loweredName.includes(search.toLowerCase())
+	})
 
-  // this all runs each time we re render
-  const searchTerm = search.toLowerCase()
-  const filteredVillagers = data.villagers.filter(villager => {
-    const lowerCaseName = villager.name['name-USen'].toLowerCase()
-    return lowerCaseName.includes(searchTerm)
-  })
+	const handleClick = (villager) => {
+		console.log(villager)
+		if(!faves.includes(villager)) {
+			// if faves doesn't include villager, add it
+			setFaves([...faves, villager])
+		} else {
+			// if faves includes villager, remove and set state
+			const villagerIndex = faves.indexOf(villager)
+			const favesState = [...faves]
+			favesState.splice(villagerIndex, 1)
+			// console.log(favesState)
+			setFaves(favesState)
+		}
+	}
 
-  return (
-    <div>
-      <label htmlFor='villager-search'>Search for a Villager:</label>
-      <input 
-        id='villager-search'
-        type='text'
-        value={search}
-        onChange={handleChange}
-      />
-
-
-        <div style={{ display: 'flex' }}>
-          <div>
-            <h1>Villagers:</h1>
-
-            <DisplayCards 
-              villagers={filteredVillagers}
-              handleClick={handleClick}
-            />
-          </div>
-
-          <div>
-            <h1>Favorites:</h1>
-
-            <DisplayCards 
-              villagers={faves}
-              handleClick={handleClick}
-            />
-          </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<h1>Animal Crossing Villager Search</h1>
+			<div>
+				<label htmlFor="villager-search">Search for a villager:</label>
+				<input
+					id="villager-search"
+					type="text"
+					placeholder="filter by villagers name"
+					value={search}
+					onChange={handleChange}
+				/>
+			</div>
+			{/* {villagerList} */}
+			<div
+				style={{
+					display:"flex"
+				}}
+			>
+				{/* <DisplayCards
+					villagers={data.villagers}
+					color={"blue"}
+				/> */}
+				<DisplayCards
+					villagers={filteredVillagers}
+					color={"red"}
+					handleClick={handleClick}
+				/>
+				<DisplayCards
+					villagers={faves}
+					color={"yellow"}
+					handleClick={handleClick}
+				/>
+			</div>
+		</>
+	)
 }
 
 export default App;
---
+++ src/components/DisplayCards.js
@@ -0,0 +1,38 @@
+export default function DisplayCards(props) {
+    const allVillagers = props.villagers.map((villager, i) => {
+        return(
+            <li
+                key={"villager-" + i}
+                style={{
+                    listStyleType:"none",
+                    border:"3px black solid",
+                    margin: "5px",
+                    width: "max-content"
+                }}
+                onClick={() => props.handleClick(villager)}
+            >
+                <img 
+                    src={villager.image_uri} 
+                    alt={"picture of " + villager.name["name-USen"]}
+                    style={{
+                        maxWidth:"250px"
+
+                    }}
+                />
+                <p>{villager.name["name-USen"]}</p>
+            </li>
+        )
+    })
+    return(
+        <>
+            <ul
+                style={{
+                    width: "300px",
+                    backgroundColor: props.color
+                }}
+            >
+                {allVillagers}
+            </ul>
+        </>
+    )
+}
