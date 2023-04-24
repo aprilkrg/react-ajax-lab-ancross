d5333a9df6e67ec2b1c931a464d648bf3e11753f
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-24 13:18:46 -0600

    clear out code in prep for lab review

--- src/App.js
+++ src/App.js
@@ -1,96 +1,15 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './App.css';
-import DisplayCards from './components/DisplayCards.js'
+// IMPORTS 
 
-function App() {
+export default function App() {
 	// STATE
-	const [data, setData] = useState({ villagers: [] })
-	const [search, setSearch] = useState("")
-	const [faves, setFaves] = useState([])
 
 	// LIFECYCLE
-	useEffect(() => {
-		try {
-			async function fetchData() {
-
-				const response = await axios.get('http://acnhapi.com/v1/villagers/')
-				// console.log("VILLAGERS:", response.data) // obj of objs
-				// Object.values() turns data into arr of objs
-				// console.log("OBJECTS:", Object.values(response.data))
-				// set state
-				setData({ villagers: Object.values(response.data) })
-			}
-			fetchData()
-		} catch (err) {
-			console.warn(err)
-		}
-	}, [])
 
 	// FUNCTIONS
-	// const villagerList = data.villagers.map((villager) => {
-	// 	return <li>{villager.name["name-USen"]}</li>
-	// })
-	const handleChange = (e) => {
-		setSearch(e.target.value)
-	}
-
-	const filteredVillagers = data.villagers.filter(villager => {
-		const loweredName = villager.name["name-USen"].toLowerCase()
-		return loweredName.includes(search.toLowerCase())
-	})
-
-	const handleClick = (villager) => {
-		console.log(villager)
-		if(!faves.includes(villager)) {
-			// if faves doesn't include villager, add it
-			setFaves([...faves, villager])
-		} else {
-			// if faves includes villager, remove and set state
-			const villagerIndex = faves.indexOf(villager)
-			const favesState = [...faves]
-			favesState.splice(villagerIndex, 1)
-			// console.log(favesState)
-			setFaves(favesState)
-		}
-	}
-
+	
 	return (
 		<>
 			<h1>Animal Crossing Villager Search</h1>
-			<div>
-				<label htmlFor="villager-search">Search for a villager:</label>
-				<input
-					id="villager-search"
-					type="text"
-					placeholder="filter by villagers name"
-					value={search}
-					onChange={handleChange}
-				/>
-			</div>
-			{/* {villagerList} */}
-			<div
-				style={{
-					display:"flex"
-				}}
-			>
-				{/* <DisplayCards
-					villagers={data.villagers}
-					color={"blue"}
-				/> */}
-				<DisplayCards
-					villagers={filteredVillagers}
-					color={"red"}
-					handleClick={handleClick}
-				/>
-				<DisplayCards
-					villagers={faves}
-					color={"yellow"}
-					handleClick={handleClick}
-				/>
-			</div>
 		</>
 	)
 }
-
-export default App;
--- src/components/DisplayCards.js
+++ src/components/DisplayCards.js
@@ -1,38 +1,7 @@
 export default function DisplayCards(props) {
-    const allVillagers = props.villagers.map((villager, i) => {
-        return(
-            <li
-                key={"villager-" + i}
-                style={{
-                    listStyleType:"none",
-                    border:"3px black solid",
-                    margin: "5px",
-                    width: "max-content"
-                }}
-                onClick={() => props.handleClick(villager)}
-            >
-                <img 
-                    src={villager.image_uri} 
-                    alt={"picture of " + villager.name["name-USen"]}
-                    style={{
-                        maxWidth:"250px"
-
-                    }}
-                />
-                <p>{villager.name["name-USen"]}</p>
-            </li>
-        )
-    })
     return(
         <>
-            <ul
-                style={{
-                    width: "300px",
-                    backgroundColor: props.color
-                }}
-            >
-                {allVillagers}
-            </ul>
+            <h2>Display Cards</h2>
         </>
     )
 }
